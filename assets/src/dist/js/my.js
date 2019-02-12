28b788d76dd0a9307fe179e7830aa8590dafc2d1
MaksimLavrenyuk <maksim.lavrenyuk@outlook.com>
2019-02-12 16:56:02 +0400

    v. 1.0

---
+++ assets/src/dist/js/my.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('[type="tel"]').mask("+7 (999) 999-99-99");
+});
+
+// модальное окно
+//= modal/modal.js
+
+// смена варианта просмотра
+//= view-switcher/view-switcher.js
--- assets/src/js/my.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('[type="tel"]').mask("+7 (999) 999-99-99");
-});
-
-
